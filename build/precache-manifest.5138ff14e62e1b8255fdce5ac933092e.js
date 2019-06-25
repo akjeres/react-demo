5225c6e5b40c08290171497acce76caf3cf33415
akjeres <akjeres@gmail.com>
2019-06-25 19:43:07 +0300

    Build added

---
+++ build/precache-manifest.5138ff14e62e1b8255fdce5ac933092e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e257b024f8db69960a4f108e01feaf02",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "6af178690a59eea41403",
+    "url": "/react-demo/static/js/2.dd29dd03.chunk.js"
+  },
+  {
+    "revision": "01c83a875064ea43de87",
+    "url": "/react-demo/static/js/main.54806962.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "de492fd61e0c836d4a159fd29a1f56ea",
+    "url": "/react-demo/static/media/hotdog.de492fd6.svg"
+  },
+  {
+    "revision": "e8c3b34635c1bf61e0220267eb261d05",
+    "url": "/react-demo/static/media/instagram.e8c3b346.svg"
+  }
+]);
